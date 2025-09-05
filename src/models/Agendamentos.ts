7800027a3d07af62a52331b53505331231a43311
caiosofiati <caiosofiatisena@hotmail.com>
2025-09-05 14:55:08 -0300

    Fixing models

--- src/models/Agendamentos.ts
+++ src/models/Agendamentos.ts
@@ -2,18 +2,18 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IAgendamentos extends Document {
   userId: mongoose.Schema.Types.ObjectId;
-  title: string;
-  date: Date; 
-  location?: string; 
-  description?: string; 
+  titulo: string;
+  data: Date; 
+  local?: string; 
+  descricao?: string; 
 }
 
 const AgendamentoSchema: Schema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
-  date: { type: Date, required: true },
-  location: { type: String },
-  description: { type: String },
+  titulo: { type: String, required: true },
+  data: { type: Date, required: true },
+  local: { type: String },
+  descricao: { type: String },
 });
 
 export default mongoose.model<IAgendamentos>('Agendamentos', AgendamentoSchema);
--- src/models/Documentos.ts
+++ src/models/Documentos.ts
@@ -2,16 +2,16 @@ import mongoose, { Schema, Document as MongooseDocument } from 'mongoose';
 
 export interface IDocumentos extends MongooseDocument {
   userId: mongoose.Schema.Types.ObjectId;
-  title: string; 
-  filePath: string;
-  fileType: string;
+  titulo: string; 
+  arqLocal: string;
+  arqTipo: string;
 }
 
 const DocumentoSchema: Schema = new Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
-  filePath: { type: String, required: true },
-  fileType: { type: String, required: true },
+  titulo: { type: String, required: true },
+  arqLocal: { type: String, required: true },
+  arqTipo: { type: String, required: true },
 });
 
 export default mongoose.model<IDocumentos>('Documentos', DocumentoSchema
